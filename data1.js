3db101ee64968d9c7c633cde3025c3b69bdde694
rusmarr <rusmarr@dunwoody.edu>
2018-02-08 21:38:53 -0600

    parks

---
+++ data1.js
@@ -0,0 +1,330 @@
+var data1=[
+    {
+      "acres": "8,323,146.48",
+      "lat":61,
+      "longitude":"-142", //Change this to longitude - long is a reserved key word in javascript
+      "name":"Wrangell.St. Elias",
+      "visitors": "79,047"
+    },
+	//Adding another instance to see if stuff works
+	{
+      "acres": "7,523,897.45",
+      "lat":67.78,
+      "longitude":"-153.3", //Change this to longitude - long is a reserved key word in javascript
+      "name":"Gates of the Arctic",
+      "visitors": "10,047"
+    },
+    {
+        
+    "acres": "4,740,911.16",
+    "lat": 63.33,
+    "longitude":"-150.5",
+    "name": "Denali",
+    "visitors": "587,412"
+        
+            },
+        
+    {
+        
+     "acres": "3,674,529.33",
+     "lat":58.5,
+     "longitude":"-155",
+     "name": "Katmai",
+     "visitors": "37,818"
+        
+            },
+        
+            {
+        
+              "acres": "3,373,063.43",
+        
+              "lat": 36.24,
+        
+              "longitude":"-116.82",
+        
+              "name": "Death Valley",
+        
+              "visitors": "1,296,283"
+        
+            },
+        
+            {
+        
+              "acres": "2,619,816.49",
+        
+              "lat": 58.5,
+        
+              "longitude":137,
+        
+              "name": "Glacier Bay",
+        
+              "visitors": "520,171"
+        
+            },
+        
+            {
+        
+              "acres": "2,219,790.71",
+        
+              "lat": 60.97,
+        
+              "longitude":"-153.42",
+        
+              "name": "Lake Clark",
+        
+              "visitors": "21,102"
+        
+            },
+        
+            {
+        
+              "acres": "1,750,716.16",
+        
+              "lat": 44.6,
+        
+              "longitude":"-110.5",
+        
+              "name": "Yellowstone",
+        
+              "visitors": "4,257,177"
+        
+            },
+        
+            {
+        
+              "acres": "1,508,968.10",
+        
+              "lat": 67.55,
+        
+              "longitude":"-159.28",
+        
+              "name": "Kobuk Valley",
+        
+              "visitors": "15,500"
+        
+            },
+        
+            {
+        
+              "acres": "1,201,647.03",
+        
+              "lat": 25.32,
+        
+              "longitude":"-80.93",
+        
+              "name": "Everglades",
+        
+              "visitors": "930,907"
+        
+            },
+        
+            {
+        
+              "acres": "1,013,128.94",
+        
+              "lat": 36.06,
+        
+              "longitude":"-112.14",
+        
+              "name": "Grand Canyon",
+        
+              "visitors": "5,969,811"
+        
+            },
+        
+            {
+        
+              "acres": "922,650.10",
+        
+              "lat": 48.8,
+        
+              "longitude":"-114",
+        
+              "name": "Glacier",
+        
+              "visitors": "2,946,681"
+        
+            },
+        
+            {
+        
+              "acres": "801,163.21",
+        
+              "lat": 47.97,
+        
+              "longitude":"-123.5",
+        
+              "name": "Olympic",
+        
+              "visitors": "3,390,221"
+        
+            },
+        
+            {
+        
+              "acres": "790,635.74",
+        
+              "lat": 29.25,
+        
+              "longitude":"-103.25",
+        
+              "name": "Big Bend",
+        
+              "visitors": "388,290"
+        
+            },
+        
+            {
+        
+              "acres": "761,747.50",
+        
+              "lat": 33.79,
+        
+              "longitude":"-115.9",
+        
+              "name": "Joshua Tree",
+        
+              "visitors": "2,505,286"
+        
+            },
+        
+            {
+        
+              "acres": "669,983.65",
+        
+               "lat":37.83,
+        
+              "longitude":"-119.5",
+        
+              "name": "Yosemite",
+        
+              "visitors": "5,028,868"
+        
+            },
+        
+            {
+        
+              "acres": "571,790.11",
+        
+              "lat": 59.92,
+        
+              "longitude":"-149.65",
+        
+              "name": "Kenai Fjords",
+        
+              "visitors": "346,534"
+        
+            },
+        
+            {
+        
+              "acres": "522,426.88",
+        
+              "lat": 48.1,
+        
+              "longitude":"-88.55",
+        
+              "name": "Isle Royale",
+        
+              "visitors": "24,966"
+        
+            },
+        
+            {
+        
+              "acres": "504,780.94",
+        
+              "lat": 35.68,
+        
+              "longitude":"-83.53",
+        
+              "name": "Great Smoky Mountains",
+        
+              "visitors": "11,312,786"
+        
+            },
+        
+            {
+        
+              "acres": "461,901.20",
+        
+              "lat": 48.7,
+        
+              "longitude":"-121.2",
+        
+              "name": "North Cascades",
+        
+              "visitors": "28,646"
+        
+            },
+        
+            {
+        
+              "acres": "404,062.63",
+        
+              "lat": 36.8,
+        
+              "longitude":"-118.55",
+        
+              "name": "Kings Canyon",
+        
+              "visitors": "607,479"
+        
+            },
+        
+            {
+        
+              "acres": "337,597.83",
+        
+              "lat": 36.43,
+        
+              "longitude":"-118.68",
+        
+              "name": "Sequoia",
+        
+              "visitors": "1,254,688"
+        
+            },
+        
+            {
+        
+              "acres": "323,431.38",
+        
+              "lat": 38.2,
+        
+              "longitude":"-109.93",
+        
+              "name": "Canyonlands",
+        
+              "visitors": "776,218"
+        
+            },
+        
+            {
+        
+              "acres": "310,043.96",
+        
+              "lat": 19.38,
+        
+              "longitude": "-155.2",
+        
+              "name": "Hawaii Volcanoes",
+        
+              "visitors": "1,887,580"
+        
+            },
+        
+            {
+        
+              "acres": "310,043.96",
+        
+              "lat": 43.73,
+        
+              "longitude":"-110.8",
+        
+              "name": "Grand Teton",
+        
+              "visitors": "3,270,076"
+        
+            }
+  ];
